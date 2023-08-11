5fffd1b9365781f5981d330f497006defd014f5c
rabnergithub <est.aramirezj@uml.edu.ni>
2023-08-11 16:13:40 -0600

    login

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,55 @@
-import './css/estilos.css';
-import { Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
+import "./css/estilos.css";
+import { Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-import { Inicio } from './pages/Inicio';
-import { Eventos } from './pages/Eventos';
-import { Sedes } from './pages/Sedes';
-import { Promociones } from './pages/Promociones';
-import { Ayuda } from './pages/Ayuda';
-import { Header } from './components/Header';
-import { Aside } from './components/Aside';
+import { Inicio } from "./pages/Inicio";
+import { Eventos } from "./pages/Eventos";
+import { Sedes } from "./pages/Sedes";
+import { Promociones } from "./pages/Promociones";
+import { Ayuda } from "./pages/Ayuda";
+import { Header } from "./components/Header";
+import { Aside } from "./components/Aside";
+import { Login } from "./pages/Login";
 
 export default function App() {
   const [mostrar, setMostrar] = useState(0);
+  const [logueado, setLogueado] = useState(localStorage.getItem("token") ? true : false);
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      setLogueado(true);
+    }
+  }, []);
+
+  const useLogout = () => {
+    localStorage.removeItem("token");
+    setLogueado(false);
+  };
+
+  
+
   return (
     <>
-      <Header
-        mostrar={mostrar}
-        setMostrar={setMostrar}
-      />
-      <Aside
-        mostrar={mostrar}
-        setMostrar={setMostrar}
-      />
+      <Header mostrar={mostrar} setMostrar={setMostrar} useLogout={useLogout} />
+      <Aside mostrar={mostrar} setMostrar={setMostrar} />
 
-
-
-      <main className='p-4 mt-4'>
+      <main className="p-4 mt-4">
         <section className="container p-4 m-auto shadow-lg">
+          {logueado ? 
+          
           <Routes>
-            <Route
-              path="/"
-              element={<Inicio />}></Route>
-            <Route
-              path="/eventos"
-              element={<Eventos />}></Route>
-            <Route
-              path="/sedes"
-              element={<Sedes />}></Route>
-            <Route
-              path="/promociones"
-              element={<Promociones />}></Route>
-            <Route
-              path="/ayuda"
-              element={<Ayuda />}></Route>
+            <Route path="/" element={<Inicio />}></Route>
+            <Route path="/eventos" element={<Eventos />}></Route>
+            <Route path="/sedes" element={<Sedes />}></Route>
+            <Route path="/promociones" element={<Promociones />}></Route>
+            <Route path="/ayuda" element={<Ayuda />}></Route>
           </Routes>
+          : <Login logueado={logueado} setLogueado={setLogueado} />}
+
 
         </section>
       </main>
 
-      <footer className='fixed bottom-0 left-0 right-0 h-auto text-white border-t-4 border-rose-500 bg-slate-500'>
+      <footer className="fixed bottom-0 left-0 right-0 h-auto text-white border-t-4 border-rose-500 bg-slate-500">
         <div className="container p-4 m-auto">
           <p className="text-center">Todos los derechos reservados</p>
         </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,37 +1,55 @@
 import { MdMenu, MdOutlinePersonPin } from 'react-icons/md';
+import { useState } from 'react';
 
 type Props = {
   mostrar: number;
   setMostrar: any;
+  useLogout: any;
 };
 
 export const Header = (props: Props) => {
-  const { mostrar, setMostrar } = props;
+  const { setMostrar } = props;
+  const [menuVisible, setMenuVisible] = useState(false);
+  const { useLogout } = props;
 
   return (
     <header className="flex items-center justify-between p-2 text-lg text-center text-white uppercase border-b-8 shadow-lg select-none bg-slate-700 border-rose-400 shadow-slate-400">
       <span
         onClick={() => setMostrar(1)}
-        className="hover:cursor-pointer hover:text-rose-400 active:text-yellow-400">
+        className="hover:cursor-pointer hover:text-rose-400 active:text-yellow-400"
+      >
         <MdMenu size={40} />
       </span>
 
       <div>
         <h1 className="font-bold">
-          Reservaciones de eventos y compras en l&iacute;nea
+          Reservaciones de eventos y compras en línea
         </h1>
-        <p>Comercial "La econ&oacute;mica"</p>
+        <p>Comercial "La económica"</p>
       </div>
 
-      <span>
-        <MdOutlinePersonPin size={40} />
-      </span>
+      {/* Icono de usuario */}
+      <div className="relative">
+        <span onClick={() => setMenuVisible(!menuVisible)}>
+          <MdOutlinePersonPin size={40} />
+        </span>
 
-      <div
-        onClick={() => setMostrar(0)}
-        className={`${
-          mostrar ? 'fixed' : 'hidden'
-        } inset-0 z-20 bg-gray-500 bg-opacity-10 backdrop-blur-sm transition-all duration-500 ease-in-out`}></div>
+        {/* Menú desplegable */}
+        {menuVisible && (
+          <div className="absolute top-full right-0 mt-4 bg-white rounded shadow-lg">
+            <ul>
+              <li className="px-5 py-2 hover:bg-gray-500 bg-gray-600">Ver perfil</li>
+              <hr />
+              
+              <li className="px-4 py-2 hover:bg-gray-500 bg-gray-600" onClick={() => useLogout()}>
+                Cerrar sesión
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+
+      
     </header>
   );
 };
